5c9e85525bdd4317d5c779258d4d6298bd1f114f
SivaRama Krishnan <sivatechnology001@gmail.com>
2026-05-22 14:48:00 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "https://meeting-video-to-action-item.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("recharts")) {
+              return "recharts";
+            }
+            if (id.includes("lucide-react")) {
+              return "lucide-react";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
